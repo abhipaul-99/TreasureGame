ab722de7bb6ab5e8508c8bef7cf886468a1b612c
Minhaj Ahmed <minhaj@Minhajs-MacBook-Air.local>
2020-04-28 02:13:08 +0530

    Updates

---
+++ precache-manifest.dc4ac237608efb0876d80a066d9e644d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad060c27c272d86fdbac0925da7a6180",
+    "url": "/TreasureGamePage/index.html"
+  },
+  {
+    "revision": "0ae6b863b590fb5c96c6",
+    "url": "/TreasureGamePage/static/css/main.7033b303.chunk.css"
+  },
+  {
+    "revision": "150db4b9251fb8c3a7e4",
+    "url": "/TreasureGamePage/static/js/2.4aaeda33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TreasureGamePage/static/js/2.4aaeda33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae6b863b590fb5c96c6",
+    "url": "/TreasureGamePage/static/js/main.7b38a47b.chunk.js"
+  },
+  {
+    "revision": "7bfe74cdfa78c13e62d0",
+    "url": "/TreasureGamePage/static/js/runtime-main.a0efc174.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TreasureGamePage/static/media/logo.5d5d9eef.svg"
+  }
+]);
